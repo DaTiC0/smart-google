e00b3549b466e77ffadde0beaf4beaeac0912029
David Dekanozishvili <dekanozishvili@gmail.com>
2021-04-02 16:22:08 +0400

    adding half of MQTT

---
+++ notifications.py
@@ -0,0 +1,28 @@
+from flask_mqtt import Mqtt
+from action_devices import rexecute
+
+mqtt = Mqtt()
+
+##################################################
+##################################################
+# @mqtt.on_log()
+# def handle_logging(client, userdata, level, buf):
+#     print(client, userdata, level, buf)
+@mqtt.on_message()
+def handle_messages(client, userdata, message):
+    print('Received message on topic {}: {}'
+          .format(message.topic, message.payload.decode()))
+    if message == 'hi':
+        print('== THIS IS NOT JOKE NO HI HERE ==')
+
+
+@mqtt.on_publish()
+def handle_publish(client, userdata, mid):
+    print('Published message with mid {}.'
+          .format(mid))
+
+
+@mqtt.on_subscribe()
+def handle_subscribe(client, userdata, mid, granted_qos):
+    print('Subscription id {} granted with qos {}.'
+          .format(mid, granted_qos))
